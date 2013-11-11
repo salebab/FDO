0b7e219508eeef2db18133f76a6618fa63cec890
Aleksandar Babic <salebab@gmail.com>
2013-11-11 10:53:41 +0100

    examples

---
+++ examples/fetch_users.php
@@ -0,0 +1,18 @@
+<?php
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
+include_once "../vendor/autoload.php";
+
+use fdo\FDO;
+
+$fdo = new FDO();
+
+$fql = "SELECT uid, name, sex FROM user WHERE uid = :uid";
+$stmt = $fdo->prepare($fql);
+$stmt->bindParam(":uid", 4, FDO::PARAM_INT);
+$stmt->execute();
+$result = $stmt->fetch(FDO::FETCH_OBJ);
+
+var_dump($result);
+
